f0f9e2ae53f51558f8664db08f63955ab1863118
Nikita <nikitademedyuk1@gmail.com>
2022-01-17 20:42:39 +0300

    Initial Commit

---
+++ GoalPostApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoalPostApp
+//
+//  Created by Никита on 17.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
