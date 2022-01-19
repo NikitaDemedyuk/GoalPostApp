138c5fdcdd4f69adf56551c78e16fd426ead1828
Nikita <nikitademedyuk1@gmail.com>
2022-01-19 19:37:50 +0300

    add view controller

---
+++ GoalPostApp/Enums/GoalType.swift
@@ -0,0 +1,13 @@
+//
+//  GoalType.swift
+//  GoalPostApp
+//
+//  Created by Никита on 19.01.22.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
